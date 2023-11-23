8599883f52df430f9cd67e2cb8cba7ab2d948c1e
andiichwannn <andiichwann@gmail.com>
2023-11-23 14:32:46 +0800

    fix fetch

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -3,6 +3,7 @@
 import '@app/diary/diary.css';
 import { useEffect, useState } from 'react'
 import axios from 'axios';
+import Link from 'next/link';
 
 export default function Diary(){
     const [judul, setJudul] = useState([])
@@ -32,15 +33,17 @@ export default function Diary(){
 
     return(
         <>
-        {judul.length>0 ? (
+        {judul.length > 0 ? (
             <ul>
                 {judul.map((item, idx)=>(
+                    <Link href={ `/diary/${item}/${isi_diary[idx]}`}>
                     <li>
                         <div className="diary-container">
                             <h1>{judul[idx]}</h1>
                             <p className="p-diary">{isi_diary[idx]}</p>
                         </div>
                     </li>
+                    </Link>
                 ))}
             </ul>
         ):
